1d4e8d564d5e348502c06835991c1e7d6f9141e8
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-08-23 19:45:27 +0530

    Use of scanner class to scan the variables on run time.

--- Scan.java
+++ Scan.java
@@ -1,5 +1,26 @@
+import java.util.* ;
+
 public class Scan{
     public static void main(String[] args){
+        System.out.println("Printing values using 'Scanner class' : ");
+        Scanner sc = new Scanner(System.in);
+
+        // Reading String Variable
+        System.out.println("Enter Your Full Name : ");
+        String name = sc.nextLine(); 
         
+
+        // Reading Integer Variable
+        System.out.print("Write down your age : ");
+        int age = sc.nextInt();
+
+        // Reading Floating Variable
+        System.out.println("Enter your Exact Weight : ");
+        float wgt = sc.nextFloat();
+
+        // Printing Values
+        System.out.println("Hi! , My name is "+name);
+        System.out.println("I'm "+age+" years old.");
+        System.out.println("My Weigth is "+wgt+" kg.");
     }
-}
+ }
