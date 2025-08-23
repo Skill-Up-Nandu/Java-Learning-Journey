59aa3960c03a771310ba1a43ec0e94c7dc5875e8
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-08-23 21:00:02 +0530

    Use different classes and function call concept

--- Conditions.java
+++ Conditions.java
@@ -1,17 +1,24 @@
 import java.util.*;
 
-public class Conditions{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("This is an EVM.");
-        System.out.println("Tell me your age : ");
-        Float age = sc.nextFloat();
-        if(age >= 18){
+class Simple{
+    void age_check(){
+    System.out.println("Tell me your age : ");
+    Scanner sc = new Scanner(System.in);
+    Float age = sc.nextFloat();
+    if(age >= 18){
             System.out.println("Go Ahead ! , You can vote.");
         }
         else{
             System.out.println("Stop! , You are not an adult.");
         }
+    }
+        
+}
+public class Conditions{
+    public static void main(String[] args){
+        System.out.println("This is an EVM.");
+        Simple obj = new Simple();
+        obj.age_check();
         
     }
 }
