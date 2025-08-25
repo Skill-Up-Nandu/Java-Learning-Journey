cf89a64caf95ddf8d75ed9e21e54014bc73df1dd
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-08-25 21:43:38 +0530

    created object for another class

---
+++ Switch.java
@@ -0,0 +1,13 @@
+class SelectPack{
+    void choices(){
+        System.out.println("Welcome to Mobile Recherge System");
+    }
+        
+}
+
+public class Switch {
+    public static void main(String[] args){
+    SelectPack obj = new SelectPack();
+    // obj.choices();  
+    }    
+}
