392ff000cb1801640050fdc84940abd84a58e90c
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-08-25 22:31:10 +0530

    added all recharge_packs details using switch case

--- Switch.java
+++ Switch.java
@@ -12,12 +12,55 @@ class SelectPack{
         
     }
 
-    void choices(){
+    int choices(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your choice");
+        System.out.println("To check details enter price");
         int choice = sc.nextInt();
+        return choice;
     }
 
+    // void select(){
+    //     System.out.println('Write "OK" to confirm');
+    // }
+    void plan599(){
+        System.out.println("You have selected Rs.599 Pack");
+        System.out.println("Plan Details - ");
+        System.out.println("- 3GB/day Data");
+        System.out.println("- Validity 84 days");
+        System.out.println("- Unlimited calls");
+        System.out.println("- 100 SMS/day");
+    }
+
+
+    void plan399(){
+        System.out.println("You have selected Rs.399 Pack");
+        System.out.println("Plan Details - ");
+        System.out.println("- 2GB/day Data");
+        System.out.println("- Validity 56 days");
+        System.out.println("- Unlimited calls");
+        System.out.println("- 100 SMS/day");
+    }
+
+    void plan199(){
+        System.out.println("You have selected Rs.199 Pack");
+        System.out.println("Plan Details - ");
+        System.out.println("- 1.5GB/day Data");
+        System.out.println("- Validity 28 days");
+        System.out.println("- Unlimited calls");
+        System.out.println("- 100 SMS/day");
+    }
+    void switchCases(int x){
+        switch (x) {
+            case 199: plan199();                
+                break;
+            case 399: plan399();
+                break;
+            case 599: plan599();
+                break;
+            default:
+                break;
+        }
+    }
     void display(){
         System.out.println("Welcome to Mobile Recherge System");
 
@@ -30,6 +73,7 @@ public class Switch {
     SelectPack obj = new SelectPack(); 
     obj.display();
     obj.rechrges(); 
-    obj.choices();
+    int choice = obj.choices();
+    obj.switchCases(choice);
     }    
 }
