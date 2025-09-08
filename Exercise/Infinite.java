d48af436ebb96bc8a795b904a8b1e0f4145c643d
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-09-08 21:22:13 +0530

    infite loop

---
+++ Exercise/Infinite.java
@@ -0,0 +1,11 @@
+public class Infinite{
+    public static void fun(){
+        int i =0;
+        do{
+            System.out.println("Im an infinite loop");
+        }while(i>=1);
+    }
+    public static void main(String[] args){
+       fun();
+    }
+}
---
+++ loops/patterns/Name.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Logic{
+    void fun(){
+
+    }
+}
+public class Name {
+    public static void main(String[] args) {
+        Logic obj = new Logic();
+        obj.fun();
+    }
+}
